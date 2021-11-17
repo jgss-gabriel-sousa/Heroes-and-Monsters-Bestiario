8d7edf50487848c99a868cabe9be91b01f930085
Gabriel Sousa <jgss.gabriel.sousa@gmail.com>
2021-11-17 02:38:17 -0300

    side buttons and responsive improve

--- monster-events.js
+++ monster-events.js
@@ -10,3 +10,33 @@ function showSpell(id){
         document.getElementById("showSpell-"+id+"-button").innerText = "+";
     }
 }
+
+
+function hideSideButtons(monster){
+    if(monster.abilities.length == 0)
+        document.getElementById("abilities-btn").style.display = "none";
+
+    if(monster.innate_spellcasting.length == 0)
+        document.getElementById("innate-btn").style.display = "none";
+
+    if(monster.spells.length == 0)
+        document.getElementById("spells-btn").style.display = "none";
+
+    if(monster.legendary_actions.length == 0)
+        document.getElementById("legendary-btn").style.display = "none";
+
+    if(monster.lair_description == "")
+        document.getElementById("lair-btn").style.display = "none";
+
+    if(monster.lair_actions.length == 0)
+        document.getElementById("lair-action-btn").style.display = "none";
+
+    if(monster.regional_effects_description.length == 0)
+        document.getElementById("regional-btn").style.display = "none";
+
+    if(monster.description.length == 0)
+        document.getElementById("description-btn").style.display = "none";
+
+
+    document.getElementById("side-buttons-section").style.display = "grid";
+}
--- monster.js
+++ monster.js
@@ -33,6 +33,8 @@ async function getMonsterData() {
         }
     }
 
+    hideSideButtons(monster);
+
     generateHTML();
 }
 
